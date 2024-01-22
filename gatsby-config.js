9e1d489d770e591c69be777a534d7be54b1a121a
MikiDix <a22miqdervil@inspedralbes.cat>
2024-01-22 17:58:55 +0100

    h

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `prova`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
